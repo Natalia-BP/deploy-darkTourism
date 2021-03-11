66b7acbd4453dcc88cee38e20f6b0cf39cafc97e
Alex Spinoza <alexandraeb20@gmail.com>
2021-03-11 21:13:33 +0000

    destacados y estilos

--- src/front/js/pages/Home.js
+++ src/front/js/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import { Destacados } from "../component/home/Destacados";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
@@ -8,7 +9,9 @@ export const Home = () => {
 		<div className="container-fluid">
 			<h1>JUMBOTRON</h1>
 			<h1>DESCRIPCION TANATOTURISMO</h1>
-			<h1>LUGARES DESTACADOS</h1>
+			<div className="fondoDestacados">
+				<Destacados />
+			</div>
 			<h1>GRID IMAGS</h1>
 		</div>
 	);
