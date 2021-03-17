d43cf054a5b942e42a7af604727ec6b9e049939f
Judycampk <judhycampk71@outlook.com>
2021-03-17 21:01:24 +0000

    cambios

--- src/front/js/component/componentes_fijos/Navbar.js
+++ src/front/js/component/componentes_fijos/Navbar.js
@@ -8,39 +8,6 @@ export const Navbar = () => {
 	const { store, actions } = useContext(Context);
 	// console.log(nick_name);
 	return (
-<<<<<<< HEAD
-		<nav className="navbar navbar-expand-lg navbar-light">
-			<button
-				className="navbar-toggler"
-				type="button"
-				data-toggle="collapse"
-				data-target="#navbarNavDropdown"
-				aria-controls="navbarNavDropdown"
-				aria-expanded="false"
-				aria-label="Toggle navigation">
-				<span className="navbar-toggler-icon" />
-			</button>
-
-			<div className="collapse navbar-collapse" id="navbarNavDropdown">
-				<ul className="navbar-nav">
-					<li className="nav-item active">
-						<Link to="/" className="nav-link">
-							Home <span className="sr-only">(current)</span>
-						</Link>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#">
-							Features
-						</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#">
-							Pricing
-						</a>
-					</li>
-				</ul>
-				<div className="nav-logo mx-auto">
-=======
 		<nav className="navbar navbar-expand-md justify-content-between">
 			<div className="container-fluid">
 				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-nav">
@@ -63,12 +30,10 @@ export const Navbar = () => {
 					</ul>
 				</div>
 				<div className="navbar-brand mx-auto d-block text-center order-0 order-md-1 w-25">
->>>>>>> 89f5027309359fc8715838238d664a065a4aad1b
 					<Link to="/">
 						<img src={Logo} className="img-fluid" width="35px" />
 					</Link>
 				</div>
-<<<<<<< HEAD
 			</div>
 			<div>
 				<i className="fas fa-heart" />
@@ -110,38 +75,6 @@ export const Navbar = () => {
 						</div>
 						{store.redirect_logout ? <Redirect to="/" /> : null}
 					</button>
-=======
-				<div className="navbar-collapse collapse dual-nav w-50 order-2">
-					<ul className="nav navbar-nav ml-auto">
-						<li className="nav-item">
-							<a className="nav-link">
-								<i className="fas fa-heart" />
-							</a>
-						</li>
-						<li className="nav-item dropdown">
-							<a className="nav-link dropdown-toggle" role="button" data-toggle="dropdown">
-								<i className="far fa-user-circle" />
-							</a>
-							<div className="dropdown-menu dropdown-menu-right" href="">
-								<a className="dropdown-item" type="button">
-									<Link to="/signup" className="dropdown-item">
-										Registrarse
-									</Link>
-								</a>
-								<a className="dropdown-item" type="button">
-									<Link to="/Login" className="dropdown-item">
-										Ingresar
-									</Link>
-								</a>
-								<a className="dropdown-item" type="button">
-									<Link to="/passwordrecover" className="dropdown-item">
-										Recuperar contraseña
-									</Link>
-								</a>
-							</div>
-						</li>
-					</ul>
->>>>>>> 89f5027309359fc8715838238d664a065a4aad1b
 				</div>
 			</div>
 		</nav>
