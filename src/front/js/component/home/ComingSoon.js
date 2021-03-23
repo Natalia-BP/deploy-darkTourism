cee53cfccdebe51e81a612e22efa0a2e8122788f
Judycampk <judhycampk71@outlook.com>
2021-03-23 02:17:59 +0000

    cambios en comingsoon

--- src/front/js/component/home/ComingSoon.js
+++ src/front/js/component/home/ComingSoon.js
@@ -4,15 +4,26 @@ export const ComingSoon = () => {
 	return (
 		<div className="row px-4">
 			<div className="column">
-				<img src="https://i.pinimg.com/originals/3a/06/d4/3a06d441cef47d33fa60467af497644b.jpg" width="100%" />
-				<img src="https://www.ucr.ac.cr/medios/fotos/pri_x-large/2015/img_2990.jpg" width="100%" />
+				<img
+					src="https://i.pinimg.com/originals/3a/06/d4/3a06d441cef47d33fa60467af497644b.jpg"
+					width="50%"
+					height="50%"
+					className="firts-photo"
+				/>
+				<img
+					src="https://www.ucr.ac.cr/medios/fotos/pri_x-large/2015/img_2990.jpg"
+					width="50%"
+					height="50%"
+					className="second-photo"
+				/>
 			</div>
 			<div className="column">
 				<img
 					src="https://elrincondelmae.files.wordpress.com/2015/05/salc3b3n-hospital-san-juan-de-dios-inicios-siglo-xx.png"
-					width="100%"
+					width="50%"
+					height="50%"
 				/>
-				<img src="https://elrincondelmae.files.wordpress.com/2015/05/2242617_0.jpg" width="100%" />
+				<img src="https://elrincondelmae.files.wordpress.com/2015/05/2242617_0.jpg" width="60%" height="40%" />
 			</div>
 		</div>
 	);
