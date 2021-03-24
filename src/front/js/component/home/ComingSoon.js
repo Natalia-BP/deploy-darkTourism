f062aadf915124663a5ddf0a031182ab2a7ffaf2
Judycampk <judhycampk71@outlook.com>
2021-03-24 02:36:43 +0000

    cambios en comingSoon y sus estilos

--- src/front/js/component/home/ComingSoon.js
+++ src/front/js/component/home/ComingSoon.js
@@ -2,29 +2,30 @@ import React from "react";
 
 export const ComingSoon = () => {
 	return (
-		<div className="row px-4 pb-5 pt-3">
-			<div className="column">
-				<h1>Próximamente</h1>
+		<div className="row px-4 pb-5 pt-3 imgstyle">
+			<div className="vertical">
+				<h1 className="text-focus-in">Próximamente</h1>
 			</div>
-			<div className="col-imag float-right d-flex flex-column">
-				<div className="column">
+			<div className="grid-container">
+				<div className="item2 itemall">
 					<img
 						src="https://i.pinimg.com/originals/3a/06/d4/3a06d441cef47d33fa60467af497644b.jpg"
-						width="50%"
-						height="50%"
-						className="firts-photo"
+						className="onephoto"
 					/>
-					<img
-						src="https://www.ucr.ac.cr/medios/fotos/pri_x-large/2015/img_2990.jpg"
-						width="50%"
-						height="50%"
-						className="second-photo"
-					/>
+				</div>
+				<div className="item3 itemall">
+					<img src="https://www.ucr.ac.cr/medios/fotos/pri_x-large/2015/img_2990.jpg" className="twophoto" />
+				</div>
+				<div className="item4 itemall">
 					<img
 						src="https://elrincondelmae.files.wordpress.com/2015/05/salc3b3n-hospital-san-juan-de-dios-inicios-siglo-xx.png"
-						width="50%"
-						height="50%"
-						className="third-photo"
+						className="twophoto"
+					/>
+				</div>
+				<div className="item5 itemall">
+					<img
+						src="https://s3.amazonaws.com/timdotcreek.com/uploads/photo/image/5bdb773f57be64533aba7bb1/Venado_Cave__Arenal.jpg"
+						className="lastphoto"
 					/>
 				</div>
 			</div>
