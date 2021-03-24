ca62151b093d00c2e2fc97592971b04f074461a2
Natalia-BP <nataliabarqueroperez@gmail.com>
2021-03-24 21:28:07 +0000

    arreglos al grid de imgs de comingSoon

--- src/front/js/component/home/ComingSoon.js
+++ src/front/js/component/home/ComingSoon.js
@@ -13,20 +13,27 @@ export const ComingSoon = () => {
 						className="onephoto"
 					/>
 				</div>
-				<div className="item3 itemall">
-					<img src="https://www.ucr.ac.cr/medios/fotos/pri_x-large/2015/img_2990.jpg" className="twophoto" />
-				</div>
-				<div className="item4 itemall">
-					<img
-						src="https://elrincondelmae.files.wordpress.com/2015/05/salc3b3n-hospital-san-juan-de-dios-inicios-siglo-xx.png"
-						className="twophoto"
-					/>
-				</div>
-				<div className="item5 itemall">
-					<img
-						src="https://s3.amazonaws.com/timdotcreek.com/uploads/photo/image/5bdb773f57be64533aba7bb1/Venado_Cave__Arenal.jpg"
-						className="lastphoto"
-					/>
+				<div className="tinyGrid">
+					<div className="img-float">
+						<div className="item3 itemall">
+							<img
+								src="https://www.ucr.ac.cr/medios/fotos/pri_x-large/2015/img_2990.jpg"
+								className="twophoto"
+							/>
+						</div>
+						<div className="item4 itemall">
+							<img
+								src="https://elrincondelmae.files.wordpress.com/2015/05/salc3b3n-hospital-san-juan-de-dios-inicios-siglo-xx.png"
+								className="twophoto"
+							/>
+						</div>
+					</div>
+					<div className="item5 itemall">
+						<img
+							src="https://s3.amazonaws.com/timdotcreek.com/uploads/photo/image/5bdb773f57be64533aba7bb1/Venado_Cave__Arenal.jpg"
+							className="lastphoto"
+						/>
+					</div>
 				</div>
 			</div>
 		</div>
